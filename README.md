06cef87a93492605f765276fad73a92f68c04934
mutabilitas <malahayatic@gmail.com>
2023-09-15 19:34:15 +0700

    Added test suite and reusable blocks

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Untuk Website: https://www.advantageonlineshopping.com/#/
 
 Memiliki minimal 5 test case,
 - 1 test case DUMMY dari import Katalon x Jira - Check Product
-- 1 test case dengan IF ELSE - Add to cart
-- 3 test case: Log in with valid credits / Log in with invalid credits / Check out
+- 1 test case dengan IF ELSE - Log in with in/valid credentials
+- 3 test case: Add to cart / Check out
 
 Memiliki 1 test suite
---
+++ Test Suites/TS_AdvantageDemo.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_AdvantageDemo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>bbdf9a4b-09c6-4551-9d24-4e40cf273cb7</testSuiteGuid>
+</TestSuiteEntity>
